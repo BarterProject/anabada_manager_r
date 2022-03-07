9f19d439c30ce2a07e0307c0be39a22add4c3888
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-03-07 18:45:02 +0900

    setting eslint, typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>아나바다 관리자 페이지</span>
+    </div>
+  );
+}
+
+export default App;
